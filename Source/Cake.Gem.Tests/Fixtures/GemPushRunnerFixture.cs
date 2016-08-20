94734b18854637924bc43bc317712d30c820e7fe
Gary Ewan Park <gep13@gep13.co.uk>
2016-08-20 20:14:01 +0100

    (GH-2) Added gem push command alias

---
+++ Source/Cake.Gem.Tests/Fixtures/GemPushRunnerFixture.cs
@@ -0,0 +1,21 @@
+﻿using Cake.Core.IO;
+using Cake.Gem.Push;
+
+namespace Cake.Gem.Tests.Fixtures
+{
+    internal sealed class GemPushRunnerFixture : GemFixture<GemPushSettings>
+    {
+        public FilePath GemFilePath { get; set; }
+
+        public GemPushRunnerFixture()
+        {
+            GemFilePath = "c:/temp/test.gem";
+        }
+
+        protected override void RunTool()
+        {
+            var tool = new GemPushRunner(FileSystem, Environment, ProcessRunner, Tools);
+            tool.Push(GemFilePath, Settings);
+        }
+    }
+}
---
+++ Source/Cake.Gem/Push/GemPushSettings.cs
@@ -0,0 +1,23 @@
+﻿namespace Cake.Gem.Push
+{
+    /// <summary>
+    /// Contains settings used by <see cref="GemPushRunner"/>.
+    /// </summary>
+    public sealed class GemPushSettings : GemSettings
+    {
+        /// <summary>
+        /// Gets or sets the name of the API Key in the credentials file which should be used for pushing the gem.
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address of the (gemcutter-compatible) host which the gem should be pushed to.
+        /// </summary>
+        public string Host { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Http Proxy which should be used for remote operations.
+        /// </summary>
+        public string HttpProxy { get; set; }
+    }
+}
